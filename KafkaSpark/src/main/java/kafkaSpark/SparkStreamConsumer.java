86291b6f24d75d86dbcdd7a39502c1cf46cb0c4f
abhisheknegi <admin@192.168.0.5>
2018-03-19 21:52:50 -0700

    Initial Commit. Created context and streaming context. No processing added so far.

--- KafkaSpark/src/main/java/kafkaSpark/SparkStreamConsumer.java
+++ KafkaSpark/src/main/java/kafkaSpark/SparkStreamConsumer.java
@@ -1,9 +1,26 @@
 package kafkaSpark;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
 public class SparkStreamConsumer {
 
 	public static void main(String[] args) {
+		
+		SparkConf sconf = new SparkConf().setAppName("SparkKafka").setMaster("local[*]");
+		
+		JavaStreamingContext jssc = new JavaStreamingContext(sconf, Durations.seconds(2));
+		
+	    jssc.start();
+	    
+	    try {
+			jssc.awaitTermination();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
+	    jssc.close();
 
 	}
 

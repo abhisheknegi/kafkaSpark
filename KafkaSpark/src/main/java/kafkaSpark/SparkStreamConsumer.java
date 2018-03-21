b432a6e385e9c8eecdb7f6a7dfb7a2cad9b73e6b
abhisheknegi <admin@192.168.0.5>
2018-03-20 22:49:48 -0700

    Add spark configuration and RDD processing

--- KafkaSpark/src/main/java/kafkaSpark/SparkStreamConsumer.java
+++ KafkaSpark/src/main/java/kafkaSpark/SparkStreamConsumer.java
@@ -1,16 +1,44 @@
 package kafkaSpark;
 
+import java.util.*;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+
+import kafka.serializer.StringDecoder;
 
 public class SparkStreamConsumer {
 
 	public static void main(String[] args) {
 		
-		SparkConf sconf = new SparkConf().setAppName("SparkKafka").setMaster("local[*]");
+		Map<String, String> prop = new HashMap<>();
+		prop.put("bootstrap.servers", "35.171.25.29:9092");
 		
-		JavaStreamingContext jssc = new JavaStreamingContext(sconf, Durations.seconds(2));
+		String topic = "testTopic";
+		Set<String> topicsSet = new HashSet<>(Arrays.asList(topic.split(",")));
+		
+		SparkConf sconf = new SparkConf().setAppName("SparkKafka").setMaster("spark://54.224.107.130:7077");
+		
+		JavaStreamingContext jssc = new JavaStreamingContext(sconf, Durations.seconds(1));
+		
+		JavaPairInputDStream<String, String> stream = KafkaUtils.createDirectStream(
+				jssc, 
+				String.class, 
+				String.class, 
+				StringDecoder.class, 
+				StringDecoder.class, 
+				prop, 
+				topicsSet);
+		
+		JavaPairDStream<String, String> words = stream.filter(x -> x._2.contains("Stormy"));
+		
+		
+		
+		words.print();
 		
 	    jssc.start();
 	    
